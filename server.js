9ebe352b8ea738fc1f3397a6b20a8385e6ff64a2
CAMERON FORSEY <cameronforsey@CAMERONs-MacBook-Air.local>
2019-10-22 19:12:36 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express= require('express');
+const port = process.env.PORT || 8000;
+
+const app = express();
+
+
+
+app.get('/', function(req, res)
+{
+    res.send("The server is running.");
+});
+
+
+
+
+
+app.listen(port, function (err) {
+if (err) {
+    console.error("Error starting the server: ", err);
+}
+console.log(`server is running at port ${port}`);
+});
